6cac2001c4023ab816cb9d44c3b8bff81d2d0e00
PavelUd <paveludin4@gmail.com>
2025-10-22 16:23:49 +0500

    first commint

---
+++ T-Tasks/T-Tasks/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static int Solve(List<string> lines)
+    {
+        // TODO: Реализация алгоритма
+        return 0;
+    }
+
+    static void Main()
+    {
+        var lines = new List<string>();
+        string line;
+        Console.WriteLine("12345");
+    }
+}
