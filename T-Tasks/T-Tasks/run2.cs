9cbf6b2d67a8f7c2cda880b1f72094e306a5465b
PavelUd <paveludin4@gmail.com>
2025-10-31 22:39:27 +0500

    solve 2

--- T-Tasks/T-Tasks/run2.cs
+++ T-Tasks/T-Tasks/run2.cs
@@ -7,54 +7,56 @@ namespace T_Tasks;
 class Program
 {
     private static Dictionary<string, HashSet<string>> _map = new();
-    private static HashSet<string> gateways = new();
+    private static HashSet<string> _gateways = new();
+
     static void Main()
     {
         string? line;
         while (!string.IsNullOrWhiteSpace(line = Console.ReadLine()))
         {
-            var tokens =  line.Split("-").ToArray();
-            if (_map.TryGetValue(tokens[0], out var value))
-            {
-                value.Add(tokens[1]);
-            }
-            else
-            {
-                _map.Add(tokens[0], [tokens[1]]);
-            }
-            if (_map.TryGetValue(tokens[1], out var value2))
-            {
-                value2.Add(tokens[0]);
-            }
-            else
-            {
-                _map.Add(tokens[1], [tokens[0]]);
-            }
+            var tokens = line.Split('-', StringSplitOptions.TrimEntries);
+            if (tokens.Length != 2) continue;
 
-            if (char.IsUpper(char.Parse(tokens[1])))
-            {
-                gateways.Add(tokens[1]);
-            }
-            if (char.IsUpper(char.Parse(tokens[0])))
-            {
-                gateways.Add(tokens[0]);
-            }
+            string a = tokens[0];
+            string b = tokens[1];
+            
+            if (!_map.TryGetValue(a, out var linksA))
+                _map[a] = linksA = new HashSet<string>();
+            linksA.Add(b);
+
+            if (!_map.TryGetValue(b, out var linksB))
+                _map[b] = linksB = new HashSet<string>();
+            linksB.Add(a);
+            
+            if (!string.IsNullOrEmpty(a) && char.IsUpper(a[0])) _gateways.Add(a);
+            if (!string.IsNullOrEmpty(b) && char.IsUpper(b[0])) _gateways.Add(b);
         }
 
-        var path = FindNearestGateway("a", _map, gateways);
+        var actions = new List<string>(); 
+
+        var path = FindNearestGateway("a", _map, _gateways);
         while (path is not null)
         {
             var pt = path.Value.Path;
             var gateway = path.Value.Gateway;
-            if (pt.Count < 2)
-            {
+
+            if (pt.Count < 2) 
                 break;
-            }
+
             var point = pt[^2];
-            Console.WriteLine($"{gateway}-{point}");
+
+            actions.Add($"{gateway}-{point}"); 
+            
             _map[point].Remove(gateway);
             _map[gateway].Remove(point);
-            path = FindNearestGateway(pt[1], _map, gateways);
+
+            var nextStart = pt.Count > 1 ? pt[1] : pt[0];
+            path = FindNearestGateway(nextStart, _map, _gateways);
+        }
+        
+        foreach (var action in actions)
+        {
+            Console.WriteLine(action);
         }
     }
 
@@ -78,25 +80,24 @@ class Program
                 nearest.Add((node, path));
                 continue;
             }
-            
+
+            if (!graph.ContainsKey(node)) continue;
+
             foreach (var neighbor in graph[node].OrderBy(n => n))
             {
                 if (visited.Add(neighbor))
                 {
                     var newPath = new List<string>(path) { neighbor };
-                    queue.Enqueue((neighbor, newPath));
+                    queue.Enqueue((newPath.Last(), newPath));
                 }
             }
         }
 
-        if (nearest.Count == 0)
-            return null;
-        
-        var result = nearest
+        if (nearest.Count == 0) return null;
+
+        return nearest
             .OrderBy(x => x.Path.Count)
             .ThenBy(x => x.Gateway)
             .First();
-
-        return result;
     }
 }
